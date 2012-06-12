6bd8c80c0009b4de0d4b89ffa133460781302fef
Jens Ulferts <j.ulferts@finn.de>
2012-06-12 13:38:41 +0200

    testing for the configurability of user deletion

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -26,11 +26,13 @@ describe UsersController do
 
   describe "GET deletion_info" do
 
-    describe "WHEN the current user is the requested user" do
+    describe "WHEN the current user is the requested user
+              WHEN the setting users_deletable_by_self is set to true" do
       let(:params) { { "id" => user.id.to_s } }
 
       before do
         @controller.stub!(:find_current_user).and_return(user)
+        Setting.stub!(:users_deletable_by_self?).and_return(true)
 
         get :deletion_info, params
       end
@@ -40,18 +42,18 @@ describe UsersController do
       it { response.should render_template("deletion_info") }
     end
 
-    describe "WHEN the current user is admin" do
+    describe "WHEN the current user is the requested user
+              WHEN the setting users_deletable_by_self is set to false" do
       let(:params) { { "id" => user.id.to_s } }
 
       before do
-        @controller.stub!(:find_current_user).and_return(admin)
+        @controller.stub!(:find_current_user).and_return(user)
+        Setting.stub!(:users_deletable_by_self?).and_return(false)
 
         get :deletion_info, params
       end
 
-      it { response.should be_success }
-      it { assigns(:user).should == user }
-      it { response.should render_template("deletion_info") }
+      it { response.response_code.should == 404 }
     end
 
     describe "WHEN the current user is the anonymous user" do
@@ -68,15 +70,49 @@ describe UsersController do
                                          :back_url => @controller.url_for({ :controller => 'users',
                                                                             :action => 'deletion_info' }) }) }
     end
+
+    describe "WHEN the current user is admin
+              WHEN the setting users_deletable_by_admins is set to true" do
+      let(:admin) { Factory.create(:admin) }
+      let(:params) { { "id" => user.id.to_s } }
+
+      before do
+        @controller.stub!(:find_current_user).and_return(admin)
+        Setting.stub!(:users_deletable_by_admins?).and_return(true)
+
+        get :deletion_info, params
+      end
+
+      it { response.should be_success }
+      it { assigns(:user).should == user }
+      it { response.should render_template("deletion_info") }
+    end
+
+    describe "WHEN the current user is admin
+              WHEN the setting users_deletable_by_admins is set to false" do
+      let(:admin) { Factory.create(:admin) }
+      let(:params) { { "id" => user.id.to_s } }
+
+      before do
+        @controller.stub!(:find_current_user).and_return(admin)
+        Setting.stub!(:users_deletable_by_admins?).and_return(false)
+
+        get :deletion_info, params
+      end
+
+      it { response.response_code.should == 404 }
+    end
   end
 
   describe "POST destroy" do
-    describe "WHEN the current user is the requested one" do
+    describe "WHEN the current user is the requested one
+              WHEN the setting users_deletable_by_self is set to true" do
       let(:params) { { "id" => user.id.to_s } }
 
       before do
         @controller.instance_eval{ flash.stub!(:sweep) }
         @controller.stub!(:find_current_user).and_return(user)
+        Setting.stub!(:users_deletable_by_self?).and_return(true)
 
         post :destroy, params
       end
@@ -85,12 +121,45 @@ describe UsersController do
       it { flash[:notice].should == I18n.t('account.deleted') }
     end
 
-    describe "WHEN the current user is the admin" do
+    describe "WHEN the current user is the requested one
+              WHEN the setting users_deletable_by_self is set to false" do
+      let(:params) { { "id" => user.id.to_s } }
+
+      before do
+        @controller.instance_eval{ flash.stub!(:sweep) }
+        @controller.stub!(:find_current_user).and_return(user)
+        Setting.stub!(:users_deletable_by_self?).and_return(false)
+
+        post :destroy, params
+      end
+
+      it { response.response_code.should == 404 }
+    end
+
+    describe "WHEN the current user is the anonymous user
+              EVEN when the setting login_required is set to false" do
+      let(:params) { { "id" => User.anonymous.id.to_s } }
+
+      before do
+        @controller.stub!(:find_current_user).and_return(User.anonymous)
+        Setting.stub!(:login_required?).and_return(false)
+
+        post :destroy, params
+      end
+
+      # redirecting post is not possible for now
+      it { response.response_code.should == 403 }
+    end
+
+    describe "WHEN the current user is the admin
+              WHEN the setting users_deletable_by_admins is set to true" do
+      let(:admin) { Factory.create(:admin) }
       let(:params) { { "id" => user.id.to_s } }
 
       before do
         @controller.instance_eval{ flash.stub!(:sweep) }
         @controller.stub!(:find_current_user).and_return(admin)
+        Setting.stub!(:users_deletable_by_admins?).and_return(true)
 
         post :destroy, params
       end
@@ -99,18 +168,20 @@ describe UsersController do
       it { flash[:notice].should == I18n.t('account.deleted') }
     end
 
-    describe "WHEN the current user is the anonymous user" do
-      let(:params) { { "id" => User.anonymous.id.to_s } }
+    describe "WHEN the current user is the admin
+              WHEN the setting users_deletable_by_admins is set to false" do
+      let(:admin) { Factory.create(:admin) }
+      let(:params) { { "id" => user.id.to_s } }
 
       before do
-        @controller.stub!(:find_current_user).and_return(User.anonymous)
-        @controller.stub!(:check_if_login_required)
+        @controller.instance_eval{ flash.stub!(:sweep) }
+        @controller.stub!(:find_current_user).and_return(admin)
+        Setting.stub!(:users_deletable_by_admins).and_return(false)
 
         post :destroy, params
       end
 
-      # redirecting post is not possible for now
-      it { response.response_code.should == 403 }
+      it { response.response_code.should == 404 }
     end
   end
 end
