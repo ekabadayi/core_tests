f8a5ad93931b6d3568668a4fd923aa84303915f2
Romano Licker <r.licker@finn.de>
2012-09-20 00:08:04 +0200

    adding wiki breadcrumb test
    * show parent wiki pages
    * show wiki menu item name instead of title if one exists
    * show current page

--- features/step_definitions/_given_steps.rb
+++ features/step_definitions/_given_steps.rb
@@ -4,3 +4,16 @@ Given /^the project "(.*?)" has a wiki menu item with the following:$/ do |proje
   item.wiki = Project.find_by_name(project_name).wiki
   item.save!
 end
+
+Given /^the project "(.*?)" has a child wiki page of "(.*?)" with the following:$/ do |project_name, parent_page_title, table|
+  wiki = Project.find_by_name(project_name).wiki
+  wikipage = Factory.build(:wiki_page, :wiki => wiki)
+
+  send_table_to_object(wikipage, table)
+
+  Factory.create(:wiki_content, :page => wikipage)
+
+  parent_page = WikiPage.find_by_wiki_id_and_title(wiki.id, parent_page_title)
+  wikipage.parent_id = parent_page.id
+  wikipage.save!
+end
